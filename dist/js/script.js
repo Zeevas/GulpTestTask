b9b8e8ca1ee87ef331bebe367b65f43c2561b4d7
Zeevas <zevdestroyer@gmail.com>
2021-01-21 16:05:24 +0300

    pixel perfect /adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,13 +7,10 @@ function testWebP(callback) {
     if (support == true) { document.querySelector('body').classList.add('webp'); }else{ document.querySelector('body').classList.add('no-webp'); }
     
     });
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value; // Display the default slider value
     
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-    }     
-    
+$(document).ready(function(){
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+    })
+});    
     
